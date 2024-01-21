04547e40832118ee4d9ebed280bf8d852a90e48e
Vladimir Palamar <vladimir.palamar@lenta.com>
2024-01-21 21:51:02 +0300

    chore: fix

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Java CI with Gradle](https://github.com/Novadiss/Auto.EighthLesson/actions/workflows/gradle.yml/badge.svg)](https://github.com/Novadiss/Auto.EighthLesson/actions/workflows/gradle.yml)
--- src/test/java/ru/netology/banklogin/page/LoginPage.java
+++ src/test/java/ru/netology/banklogin/page/LoginPage.java
@@ -1,5 +1,6 @@
 package ru.netology.banklogin.page;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import ru.netology.banklogin.data.DataHelper;
 
@@ -13,10 +14,9 @@ public class LoginPage {
     private final SelenideElement loginButton = $("[data-test-id=action-login]");
     private final SelenideElement errorNotification = $("[data-test-id='error-notification'] .notification__content");
 
-    public void verifyErrorNotification(String expectedText) {
+    public void verifyErrorNotification(String expectedText){
         errorNotification.shouldHave(exactText(expectedText)).shouldBe(visible);
     }
-
     public VerificationPage validLogin(DataHelper.AuthInfo info) {
         loginField.setValue(info.getLogin());
         passwordField.setValue(info.getPassword());
